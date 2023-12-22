97e8154b18d6e82351e27aca21c0839015a21f08
vikash kumar <vk1395631@gmail.com>
2023-12-22 23:05:47 +0530

    learn props, props type and defalut props and importing for one module to another after lec 6

---
+++ src/module1.mjs
@@ -0,0 +1,5 @@
+import  def, {d,b,c} from './module2.mjs';
+console.log(d);
+console.log(def);
+console.log(b);
+console.log(c);
---
+++ src/module2.mjs
@@ -0,0 +1,9 @@
+const a="rohan";
+const b="deepak";
+const c="ravi";
+const d="vikash"
+
+export default a;
+export {b};
+export {c};
+export {d};
