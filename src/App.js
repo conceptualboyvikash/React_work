b64afd1d1a75f9f8bb0cccae11c5d3736e31462c
vikash kumar <vk1395631@gmail.com>
2023-12-23 13:19:24 +0530

    here we learn to use state and hook uses after lec 10

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import Accord from './Accord';
 import './App.css';
 import Navbar from './components/Navbar';
-import Textform from './components/Textform';
+
 
 function App() {
   return (
    <>
    <Navbar  title="Text-Magic" about="mobile no" />
     {/* <Navbar /> */}
-    <div className="container my-3">
-      <Textform heading="by vikash kumar" />
-    </div>
-   </>
+    
+       {/* <Accord/> */}
+       <Accord/>
+ 
+      
+    </>
   );
 }
 
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,78 +1,92 @@
-import React, {useState} from 'react'
-
+import React, { useState } from "react";
 
 export default function Textform(props) {
-    const handleclick=(event)=>{
-        // console.log("onclick on");
-        setText(event.target.value);
-        
-    }
-    const handleclick1=(event)=>{
-        // console.log("onclick on");
-        fsetText(event.target.value);
-    }
-    
+  const handleclick = (event) => {
+    // console.log("onclick on");
+    setText(event.target.value);
+  };
+  const handleclick1 = (event) => {
+    // console.log("onclick on");
+    fsetText(event.target.value);
+  };
 
-    const handleclick2=(event)=>{
-        // console.log("onclick on");
-        rsetText(event.target.value);
-    }
-        
-  
-    const handleUpClick=(event)=>{
-        const newtext=text.toUpperCase();
-        setText(newtext);
-      
-    }
-    const handleDownClick=(event)=>{
-        const newtext=text.toLowerCase();
-        setText(newtext);
-      
-    }
-    const clearText=(event)=>{
-        const newtext=('');
-        setText(newtext);
-      
-    }
-    const ReplaceText=(event)=>{
-        const newtext=text.replaceAll(ftext,rtext);
-        setText(newtext);
-      
-    }
-    
-    const [text, setText]=useState('');
-    const [ftext, fsetText]=useState('');
-    const [rtext, rsetText]=useState('');
-    
-    // text="conceput_clear" //wrong way 
-    // setText("coneptu_clar") //correct way
+  const handleclick2 = (event) => {
+    // console.log("onclick on");
+    rsetText(event.target.value);
+  };
+
+  const handleUpClick = (event) => {
+    const newtext = text.toUpperCase();
+    setText(newtext);
+  };
+  const handleDownClick = (event) => {
+    const newtext = text.toLowerCase();
+    setText(newtext);
+  };
+  const clearText = (event) => {
+    const newtext = "";
+    setText(newtext);
+  };
+  const ReplaceText = (event) => {
+    const newtext = text.replaceAll(ftext, rtext);
+    setText(newtext);
+  };
+
+  const [text, setText] = useState("");
+  const [ftext, fsetText] = useState("");
+  const [rtext, rsetText] = useState("");
+
+  // text="conceput_clear" //wrong way
+  // setText("coneptu_clar") //correct way
   return (
     <>
-    <div>
+      <div>
         <div className="mb-3">
-        <h1>Enter you Text Below </h1>
-        <textarea className="form-control"  value={text} onChange={handleclick} rows="8"></textarea>
+          <h1>Enter you Text Below </h1>
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleclick}
+            rows="8"
+          ></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
-        <button className="btn btn-primary mx-2" onClick={handleDownClick}>Convert to LowerCase</button>
-        <button className="btn btn-primary mx-2" onClick={clearText}>ClearText</button>
+        <button className="btn btn-primary" onClick={handleUpClick}>
+          Convert to UpperCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleDownClick}>
+          Convert to LowerCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={clearText}>
+          ClearText
+        </button>
 
-
-        <button className="btn btn-primary mx-2 my-2" onClick={ReplaceText}>ReplaceAllText</button>
-        <input className="mx-1" type="text"  value={ftext} onChange={handleclick1} size="10"/>
-        <input className="mx-1" type="text"  value={rtext} onChange={handleclick2} size="10" />
-
-       
-    </div>
-    <div className="container my-4">
-      <h2>Text Summary</h2>
-      <p>{text.split(" ").length} words and {text.length}</p>
-      <p>{0.008*text.split(" ").length} time take to read</p>
-      <h2>preview</h2>
-      <p>{text}</p>
-      
+        <button className="btn btn-primary mx-2 my-2" onClick={ReplaceText}>
+          ReplaceAllText
+        </button>
+        <input
+          className="mx-1"
+          type="text"
+          value={ftext}
+          onChange={handleclick1}
+          size="10"
+        />
+        <input
+          className="mx-1"
+          type="text"
+          value={rtext}
+          onChange={handleclick2}
+          size="10"
+        />
       </div>
-
+      <div className="container my-4">
+        <h2>Text Summary</h2>
+        <p>
+          {text.split(" ").length} words and {text.length}
+        </p>
+        <p>{0.008 * text.split(" ").length} time take to read</p>
+        <h2>preview</h2>
+        <p>{text}</p>
+      </div>
     </>
-  )
+  );
 }
